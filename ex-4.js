cff648e463a200f79f6f7b1a146af5c7b626c16d
Jakkaphob <jkpbeam@gmail.com>
2024-05-15 14:39:24 +0700

    feat: add Javascript conditional ex1-7 as feature/beam

--- ex-4.js
+++ ex-4.js
@@ -3,3 +3,11 @@
 let lightBulbStatus = "On";
 
 //Start coding here
+switch (lightBulbStatus) {
+  case "On":
+    console.log("Light bulb is On.");
+    break;
+  case "Off":
+    console.log("Light bulb is Off.");
+    break;
+}
